feada58b4e9a94ba99fc64f96ec5c5f12c3e8f6c
sankalp142002 <83068901+sankalp142002@users.noreply.github.com>
2022-04-19 03:01:29 +0530

    Add files via upload

---
+++ Project/fooditem.js
@@ -0,0 +1,130 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Lazeez Bhuna Murgh Biryani',
+    category : 'biryani',
+    rating : 4.7,
+    price: 450,
+    img: 'images/Lazeez Bhuna Murgh.webp',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Shahi Dum Gosht Biryani',
+    category : 'biryani',
+    rating : 4.9,
+    price: 480,
+    img: 'images/Shahi Dum Gosht.webp',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Murgh Tikka Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 380,
+    img: 'images/Murgh Tikka.webp',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Murgh Afghani Tikka Biryani',
+    category : 'biryani',
+    rating : 4.5,
+    price: 450,
+    img: 'images/Murgh Afghani Tikka.webp',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Do Gosht Ki Biryani',
+    category : 'biryani',
+    rating : 4.0,
+    price: 480,
+    img: 'images/Do Gosht Biryani.webp',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Subz E Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 300,
+    img: 'images/Subz E Biryani.webp',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Hyderabadi Mutton Biryani + Murgh Kefta (9pcs)',
+    category : 'biryani',
+    rating : 4.8,
+    price: 680,
+    img: 'images/Hyderabadi Mutton Biryani (Spicy Dum Gosht, Serves 1) + Murgh Kefta (9pcs).webp',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Dum Gosht Biryani + Murgh Kefta-Full',
+    category : 'biryani',
+    rating : 4.7,
+    price: 650,
+    img: 'images/Dum Gosht (Serves 1) + Murgh Kefta-Full ( Serves 1).webp',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Murgh Koobideh',
+    category : 'biryani',
+    rating : 4.6,
+    price: 160,
+    img: 'images/Murgh Koobideh.webp',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Murgh Kefta Full',
+    category : 'biryani',
+    rating : 4.3,
+    price: 200,
+    img: 'images/Murgh Kefta Full.webp',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Beetroot Kebabs',
+    category : 'biryani',
+    rating : 3.5,
+    price: 100,
+    img: 'images/Beetroot Kebabs.webp',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Falafal E Khas (veg)',
+    category : 'biryani',
+    rating : 4.3,
+    price: 140,
+    img: 'images/Falafal E Khas.webp',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Saffron  Phirni',
+    category : 'biryani',
+    rating : 4.8,
+    price: 120,
+    img: 'images/Saffron  Phirni.webp',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Rose Phirni',
+    category : 'biryani',
+    rating : 4.3,
+    price: 120,
+    img: 'images/Rose Phirni.webp',
+    quantity: 1
+},
+]
+
+export {foodItem};
